eabc7a092495303d80f174ec4ca8374a8d80916b
Arlyn Cotrado <alejandra321.any@gmail.com>
2020-01-10 21:20:25 -0500

    formulario datos de alumno

--- app/src/main/java/com/example/u1ejerciciopropuesto1/MainActivity.java
+++ app/src/main/java/com/example/u1ejerciciopropuesto1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.u1ejerciciopropuesto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculadora(View view) {
+        startActivity(new Intent(this, Calculadora.class));
+    }
+    public void salir(View view){
+        finish();
+    }
+
+    public void alumno(View view) {
+        startActivity(new Intent(this, Alumno.class));
     }
 }
