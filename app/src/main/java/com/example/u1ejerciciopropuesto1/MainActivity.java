ad78dbb748296795dc2268605eff2da29841606a
Arlyn Cotrado <alejandra321.any@gmail.com>
2020-01-10 23:56:59 -0500

    funcionalidad de la calculadora

--- app/src/main/java/com/example/u1ejerciciopropuesto1/MainActivity.java
+++ app/src/main/java/com/example/u1ejerciciopropuesto1/MainActivity.java
@@ -2,16 +2,26 @@ package com.example.u1ejerciciopropuesto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnUno,btnDos,btnTres,btnCuatro,btnCinco,btnSeis,btnSiete,btnOcho,
+            btnNueve,btnSuma,btnResta,btnMultiplica,btnDivide,btnClean,btnBorrar,btnPunto,btnIgual;
+    TextView Resultado;
+    double resultado;
+    String operador, mostrar , reserva;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     public void calculadora(View view) {
